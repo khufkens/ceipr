6f94048c1eaa919d2facacfa2172d8c23da2db18
khufkens <koen.hufkens@gmail.com>
2018-08-09 15:31:14 +0200

    first commit

---
+++ R/ceip_download.r
@@ -0,0 +1,91 @@
+#' Download and compile emission data from
+#' the Centre on Emission Inventories and Projections (CEIP)
+#'
+#' @param pollutants character string with pollutants
+#' (CO, NH3, NMVOC, NOx, PM2_5, PM10, PMcoarse, SOx)
+#' @param sectors different societal sectors for which to compile the data
+#' as referenced by their abbreviation (A - N & NT)
+#' @param out_dir output directory
+#' @param base_url source url to download data from
+#' @return geotiff written to disk
+#' @keywords emission, voc
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#' ceip_download()
+#'}
+
+ceip_download <- function(pollutants = c("SOx"),
+                         sectors = "A", #c(LETTERS[1:13],"NT"),
+                         out_dir = "~",
+                         base_url = "http://webdab1.umweltbundesamt.at/download/gridding2018/"){
+
+  # clean tempdir of stray txt files
+  # as tempdir content persists throught an R session
+  # [warning: might fuck up some other stuff]
+  file.remove(list.files(tempdir(),"*.txt", full.names = TRUE))
+  file.remove(list.files(tempdir(),"*.tif", full.names = TRUE))
+
+  # split out year directories from download path
+  # dynamically coded to adjust to new data automatically
+  # could be static for shorter codebase
+  years <- xml2::read_html(base_url)
+  years <- data.frame(rvest::html_table(years))$Name
+  years <- as.numeric(gsub("/",
+                           "",
+                           years[grep("^[0-9]",years)]))
+
+  # list all ceip files in your download path
+  files <- list.files(tempdir(), "*.txt", full.names = TRUE)
+
+  # convert everything to geotiff files
+  invisible(lapply(pollutants, function(pollutant){
+    lapply(years, function(year){
+
+      # get the zipfile for a particular pollutant
+      zip_files <- xml2::read_html(paste(base_url, year, sep = "/"))
+      zip_files <- data.frame(rvest::html_table(zip_files))$Name
+      zip_file <- zip_files[grep(paste0(pollutant, ".*.zip"), zip_files)]
+
+      # download and unzip the zipfile
+      download.file(url = paste(base_url, year, zip_file, sep = "/"),
+                    destfile = file.path(tempdir(), "tmp.zip"),
+                    quiet = TRUE)
+      unzip(file.path(tempdir(), "tmp.zip"), exdir = tempdir())
+
+      # convert only required data (sectors) to geotiffs
+      txt_files <- list.files(tempdir(), paste0(pollutant,".*.txt"),
+                              full.names = TRUE)
+      txt_files <- txt_files[grep(paste(paste0("_",sectors,"_"),
+                                        collapse = "|"),
+                                  txt_files)]
+
+      lapply(txt_files, function(txt_file){
+        try(ceip_grid(file = txt_file,
+                             out_dir = tempdir(),
+                             internal = FALSE))
+      })
+
+      # cleanup
+      file.remove(txt_files)
+    })
+
+    # by pollutant and sector, get all files
+    # and put into raster stack
+    lapply(sectors, function(sector){
+
+      # list tif files by sector (sorted by year by default)
+      tif_files <- list.files(tempdir(),paste0("^.*_",sector,"_.*\\.tif$"),
+                              full.names = TRUE)
+
+      # write stack to file
+      raster::writeRaster(raster::stack(tif_files),
+                  file.path(out_dir, paste0(pollutant,"_",sector,".tif")),
+                  overwrite = TRUE)
+
+      # cleanup
+      file.remove(tif_files)
+    })
+  }))
+}
---
+++ R/ceip_grid.r
@@ -0,0 +1,59 @@
+#' Converts text based data from the 
+#' Centre on Emission Inventories and Projections (CEIP)
+#' into raster data (geotiff or raster object in the R workspace)
+#'
+#' @param file CEIP txt data file
+#' @param out_dir output directory where to store data when writing to disk
+#' @param internal bolean TRUE / FALSE, write output to disk or return to
+#' R workspace
+#' @return geotiff written to disk or raster data returned to R workspace
+#' @keywords emission, voc
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#' ceip_grid()
+#'}
+
+ceip_grid <- function(file,
+                      out_dir = tempdir(),
+                      internal = FALSE){
+  
+  # read in data
+  df <- read.table(file,
+                   sep = ";",
+                   header = FALSE,
+                   stringsAsFactors = FALSE)
+  
+  # Add variable names (hopefully consistent across
+  # all files)
+  names(df) <-  c("iso2", "year", "sector",
+                  "pollutant", "longitude",
+                  "latitude", "unit", "emission")
+  
+  # convert to spatial data frame
+  sp::coordinates(df) <- ~longitude+latitude
+  
+  # create the grid as defined here:
+  # http://webdab1.umweltbundesamt.at/download/01GridData/\
+  # EMEP_gridding_system_documentation.pdf
+  
+  # first create an arbitrary WGS84 grid
+  r <- raster::raster(ncols = 1200, nrows = 520)
+  
+  # reassign the extent
+  raster::extent(r) <- c(-30,90,30,82)
+  
+  # fill with the corresponding values
+  # from the spatial data frame (rasterize)
+  r <- raster::rasterize(df, r, field = "emission")
+  
+  if (internal){
+    return(r)
+  } else {
+    raster::writeRaster(r,
+                        paste0(tools::file_path_sans_ext(file),".tif"),
+                        overwrite = TRUE,
+                        options = "COMPRESS=DEFLATE")
+  }
+}
