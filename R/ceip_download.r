9bbb8d0c5bdb10549391a1c30b3253966b4caf41
Koen Hufkens <koen.hufkens@gmail.com>
2018-08-12 00:51:08 +0200

    refactoring using Thomas' codebase using a tidyverse approach not a geospatial one as before

--- R/ceip_download.r
+++ R/ceip_download.r
@@ -1,107 +1,70 @@
-#' Download and compile emission data from
-#' the Centre on Emission Inventories and Projections (CEIP)
+#' Download zipped Centre on Emission Inventories and Projections (CEIP) data
+#'
+#' The download routine will recreate the CEIP file structure in the output
+#' directory (sorting files in folders by year).
 #'
 #' @param pollutants character string with pollutants
 #' (CO, NH3, NMVOC, NOx, PM2_5, PM10, PMcoarse, SOx)
-#' @param sectors different societal sectors for which to compile the data
-#' as referenced by their abbreviation (A - N & NT)
-#' @param out_dir output directory
+#' @param years years to download (all = 2000:2016)
+#' @param out_dir output directory (default = tempdir())
 #' @param base_url source url to download data from
-#' @return geotiff written to disk
+#' @return Downloads ceip zip files into the output directory. CRAN policy does
+#' not allow default settings to write to the userspace so tempdir() is the
+#' default. However, a different directory can be used for caching.
 #' @keywords emission, voc
 #' @export
 #' @examples
 #'
 #' \dontrun{
-#' ceip_download()
+#' ceip_download(pollutant = "SOx",
+#'               year = 2000)
 #'}
 
 ceip_download <- function(
-  pollutants = c("SOx"),
-  sectors = "A", #c(LETTERS[1:13],"NT"),
-  out_dir = "~",
-  base_url = "http://webdab1.umweltbundesamt.at/download/gridding2018/"){
+  pollutant = c("SOx", "NOx"),
+  year = 2000:2016,
+  out_dir = tempdir(),
+  base_url = "http://webdab1.umweltbundesamt.at/download/gridding2018/",
+  silent = FALSE){
 
-  # clean tempdir of stray txt files
-  # as tempdir content persists throught an R session
-  # [warning: might fuck up some other stuff]
-  file.remove(list.files(tempdir(),"*.txt", full.names = TRUE))
-  file.remove(list.files(tempdir(),"*.tif", full.names = TRUE))
+  # check if output directory exists
+  if(!dir.exists(out_dir)){
+    stop("Specified output directory does not exist, check out_dir parameter!")
+  }
 
-  # split out year directories from download path
-  # dynamically coded to adjust to new data automatically
-  # could be static for shorter codebase
-  years <- xml2::read_html(base_url)
-  years <- data.frame(rvest::html_table(years))$Name
-  years <- as.numeric(gsub("/",
-                           "",
-                           years[grep("^[0-9]",years)]))
+  # suppress (empty) output
+  invisible(
+    purrr::map(year,function(y) {
+      purrr::map(pollutant, function(p) {
 
-  # list all ceip files in your download path
-  files <- list.files(tempdir(), "*.txt", full.names = TRUE)
+        # format input file
+        file <- ceipr::ceip_zip_file(path = base_url,
+                      year = y,
+                      pollutant = p)
 
-  # convert everything to geotiff files
-  invisible(lapply(pollutants, function(pollutant){
-    lapply(years, function(year){
+        # format file destination
+        dest_file <- file.path(out_dir, y, basename(file))
 
-      # get the zipfile for a particular pollutant
-      zip_files <- xml2::read_html(paste(base_url, year, sep = "/"))
-      zip_files <- data.frame(rvest::html_table(zip_files))$Name
-      zip_file <- zip_files[grep(paste0(pollutant, ".*.zip"), zip_files)]
+        # create directory structure if it does not exist
+        if(!dir.exists(dirname(dest_file))){
+          dir.create(dirname(dest_file))
+        }
 
-      # download and unzip the zipfile
-      utils::download.file(url = paste(base_url, year, zip_file, sep = "/"),
-                    destfile = file.path(tempdir(), "tmp.zip"),
-                    quiet = TRUE)
+        # some feedback
+        message(paste("Downloading:", basename(file)))
 
-      # check if the file was create if not raise a warning
-      if(!file.exists(file.path(tempdir(), "tmp.zip"))){
-        warning(paste0("No data available for pollutant: ",
-                       pollutant,
-                       " and year: ",
-                       year,
-                       " results will be unreliable!"))
-        return(NULL)
-      }
+        # download a particular fiel
+        status <- httr::GET(url = file,
+                               httr::write_disk(path = dest_file,
+                                                overwrite = TRUE),
+                               httr::progress())
 
-      # if exist, continue to unzip etc
-      utils::unzip(file.path(tempdir(), "tmp.zip"), exdir = tempdir())
-
-      # convert only required data (sectors) to geotiffs
-      txt_files <- list.files(tempdir(), paste0(pollutant,".*.txt"),
-                              full.names = TRUE)
-      txt_files <- txt_files[grep(paste(paste0("_",sectors,"_"),
-                                        collapse = "|"),
-                                  txt_files)]
-
-      lapply(txt_files, function(txt_file){
-        try(ceip_grid(file = txt_file,
-                             internal = FALSE))
+        # error / stop on 400 error
+        if(httr::http_error(status)){
+          warning(paste("Requested file:",
+                          basename(file),
+                          "is not found. Check connection or parameters!"))
+        }
       })
-
-      # cleanup
-      file.remove(txt_files)
-    })
-
-    # by pollutant and sector, get all files
-    # and put into raster stack
-    lapply(sectors, function(sector){
-
-      # list tif files by sector (sorted by year by default)
-      tif_files <- list.files(tempdir(),paste0("^.*_",sector,"_.*\\.tif$"),
-                              full.names = TRUE)
-
-      # add proper names to stack layers
-      s <- raster::stack(tif_files)
-      names(s) <- years
-
-      # write stack to file
-      raster::writeRaster(s,
-                  file.path(out_dir, paste0(pollutant,"_",sector,".tif")),
-                  overwrite = TRUE)
-
-      # cleanup
-      file.remove(tif_files)
-    })
   }))
 }
---
+++ R/helper_functions.r
@@ -0,0 +1,126 @@
+#' Formats the name of the CEIP zip file location
+#'
+#' location is either remote or local as defined
+#' by the path variable
+#'
+#' @param path CEIP sectors
+#' @param year year to download
+#' @param pollutant different pollutants
+#' @return zip file location string
+#' @export
+ceip_zip_file <- function(path, year, pollutant) {
+  paste0(path,'/',year,'/',pollutant,'_2018_GRID_',year,'.zip')
+}
+
+#' Formats the name of the CEIP file inside the zip file
+#'
+#' @param sector CEIP sectors
+#' @param pollutant different pollutants
+#' @param year year to download
+#' @return url location
+#' @export
+ceip_data_file <- function(year, pollutant, sector) {
+
+  # read in meta data (sectors)
+  meta_data <- utils::read.table(sprintf("%s/extdata/ceip_meta_data.csv",
+                                         path.package("ceipr")),
+                                 sep = ",",
+                                 header = TRUE,
+                                 stringsAsFactors = FALSE)
+
+  # create data file string
+  data_file <- paste0(pollutant,'_',sector,'_',
+        gsub(" ", "", meta_data$sector[which(meta_data$abbreviation == sector)]),
+        '_2018_GRID_',year,'.txt')
+
+  # if sectior is "NT" (National Total) strip long form name
+  if(sector == "NT"){
+    return(gsub("_NationalTotal","",data_file))
+  } else {
+    return(data_file)
+  }
+}
+
+#' Reads internal file from a zip file (unzip in memory)
+#'
+#' @param path location of a zip file (url / or local path)
+#' @param pollutant different pollutants
+#' @param year year to download
+#' @return reads a specific file
+#' @export
+ceip_read_zip <- function(zip, filename) {
+
+  # read_delim() settings
+  # column names of the ceip files
+  csv_column_names <- c("iso2", "year", "sector",
+                        "pollutant", "longitude",
+                        "latitude", "unit", "emission")
+
+  # column formats of the ceip files
+  csv_col_types <- readr::cols(
+    iso2 = readr::col_character(),
+    year = readr::col_integer(),
+    sector = readr::col_character(),
+    pollutant = readr::col_character(),
+    longitude = readr::col_number(),
+    latitude = readr::col_number(),
+    unit = readr::col_character(),
+    emission = readr::col_double()
+  )
+
+  # comment, delimitir and locale settigns
+  delim <- ";"
+  comment <- "#"
+  us_locale <- locale(decimal_mark = '.',
+                      grouping_mark = ',')
+
+  # read in thet data directly from zip file
+  readr::read_delim(
+    unz(zip, filename),
+    comment = comment,
+    col_names = csv_column_names,
+    col_types = csv_col_types,
+    delim = delim,
+    locale = us_locale
+  )
+
+  # explicitely closed to avoid warnings due to time-out
+  closeAllConnections()
+}
+
+#' converts ceip subset data frame to a raster
+#'
+#' Never to be used stand alone, only as a helper function
+#'
+#' @param df subset of a full ceip data frame as returned by ceip_read()
+#' @return raster layer
+#' @export
+#'
+convert_to_raster <- function(df) {
+
+  # create the grid as defined here:
+  # http://webdab1.umweltbundesamt.at/download/01GridData/
+  # EMEP_gridding_system_documentation.pdf
+
+  # first create an arbitrary WGS84 grid
+  r <- raster::raster(ncols = 1200, nrows = 520)
+
+  # reassign the extent
+  raster::extent(r) <- c(-30,90,30,82)
+
+  # if no data is provided, return an empty
+  # raster (helpful for failed reads etc)
+  # else fill the raster with values
+  if(nrow(df)==0 | is.null(df)){
+    return(r)
+  } else {
+
+  # assign coordinates to create a spatial data frame
+  sp::coordinates(df) <- ~longitude+latitude
+
+  # fill with the corresponding values
+  # from the spatial data frame (rasterize)
+  r <- raster::rasterize(df, r, field = "emission")
+  return(r)
+  }
+}
